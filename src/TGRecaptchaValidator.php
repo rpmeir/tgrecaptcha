6fc87f188208389c51475c3e8cdbd809957249c5
Rodrigo Pires Meira <rpmeir@gmail.com>
2023-08-26 22:59:53 -0300

    fix: Add requirement and use exception

--- src/TGRecaptchaValidator.php
+++ src/TGRecaptchaValidator.php
@@ -3,18 +3,20 @@
 namespace Rpmeir\TGRecaptcha;
 
 use Adianti\Validator\TFieldValidator;
+use Exception;
+use ReCaptcha\ReCaptcha;
 
 class TGRecaptchaValidator extends TFieldValidator
 {
     public function validate($label, $value, $params = null)
     {
-        if(!value)
+        if(!$value)
         {
             throw new Exception("O campo {$label} é obrigatório");
         }
 
         $recaptcha_config = require_once('app/config/recaptcha.php');
-        $recaptcha = new \ReCaptcha\ReCaptcha($recaptcha_config['chave_secreta']);
+        $recaptcha = new ReCaptcha($recaptcha_config['chave_secreta']);
         $resp = $recaptcha->setExpectedHostname($_SERVER["SERVER_NAME"])
             ->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
 
